a19faf6bdb7bf9b7242592918ac3f719b89c9568
choplua <chrischris1176@gmail.com>
2024-02-26 23:02:14 -0500

    added squares(and fixed css issues). turn and clicking

--- js/main.js
+++ js/main.js
@@ -2,9 +2,39 @@ console.log('Your JS is linked up. Be the person you needed when you were little
 
 /*----- constants -----*/
 /*----- app's state (variables) -----*/
+let board;
+let turn = 'X';
 /*----- cached element references -----*/
+const squares = Array.from(document.querySelectorAll('#board div'));
 /*----- event listeners -----*/
+document.getElementById('board').addEventListener('click', handleTurn);
 /*----- functions -----*/
+function init() {
+    board = [
+    '', '', '',
+    '', '', '',
+    '', '', ''
+    ];
+};
+function render() {
+    board.forEach(function(mark, index){
+        console.log(mark, index);
+        squares[index].textContent = mark;
+    });
+}
+function handleTurn(event) {
+    let idx = squares.findIndex(function(square) {
+    return square === event.target;
+    });
+    board[idx] = turn;
+    
+    console.log(board);
+ };
+
+
+
+init();
+render();
 
 
 
